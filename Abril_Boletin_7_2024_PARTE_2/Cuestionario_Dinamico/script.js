3f023e25c823705f6231b9fa90b3ed650c6a04c7
nicoleabril <nicole.abrilc@ucuenca.edu.ec>
2024-05-02 07:15:14 -0500

    Actualizacion

--- Abril_Boletin_7_2024_PARTE_2/Cuestionario_Dinamico/script.js
+++ Abril_Boletin_7_2024_PARTE_2/Cuestionario_Dinamico/script.js
@@ -6,7 +6,7 @@ function generateQuestions() {
     for (var i = 1; i <= numQuestions; i++) {
         var questionDiv = document.createElement('div');
         questionDiv.innerHTML = `
-            <label for="question${i}">Pregunta ${i}:</label>
+            <label  for="question${i}" >Pregunta ${i}:</label>
             <input type="text" id="question${i}" name="question${i}" required>
             <select id="questionType${i}" onchange="showOptions(${i})">
                 <option value="text">Pregunta de texto</option>
@@ -16,7 +16,7 @@ function generateQuestions() {
             <div id="optionsContainer${i}" style="display: none;">
                 <label for="numOptions${i}">Número de opciones múltiples:</label>
                 <input type="number" id="numOptions${i}" name="numOptions${i}" min="1" max="10">
-                <button type="button" onclick="addMultipleOptions(${i})">Agregar</button>
+                <button class="buttonAgregar" type="button" onclick="addMultipleOptions(${i})">Agregar</button>
                 <div id="multipleOptions${i}" style="display: none;"></div>
             </div>
             <br>
@@ -69,31 +69,86 @@ function submitForm() {
         var questionType = document.getElementById('questionType' + i).value;
 
         var questionDiv = document.createElement('div');
-        questionDiv.innerHTML = `
-            <label>${question}</label>
-            <br>
-        `;
+        questionDiv.classList.add('form-group');
+
+        // Agregar clase según el tipo de pregunta
+        if (questionType === 'text') {
+            questionDiv.classList.add('text-question');
+        } else if (questionType === 'truefalse') {
+            questionDiv.classList.add('truefalse-question');
+        } else if (questionType === 'multiplechoice') {
+            questionDiv.classList.add('multiplechoice-question');
+        }
+
+        var labelQuestion = document.createElement('label');
+        labelQuestion.textContent = question;
+        questionDiv.appendChild(labelQuestion);
 
         if (questionType === 'text') {
-            questionDiv.innerHTML += `<input type="text" id="answer${i}" name="answer${i}" required>`;
+            var inputText = document.createElement('input');
+            inputText.type = 'text';
+            inputText.id = 'answer' + i;
+            inputText.name = 'answer' + i;
+            inputText.required = true;
+            questionDiv.appendChild(inputText);
         } else if (questionType === 'truefalse') {
-            questionDiv.innerHTML += `
-                <input type="radio" id="answer${i}_true" name="answer${i}" value="true" required>
-                <label for="answer${i}_true">Verdadero</label>
-                <input type="radio" id="answer${i}_false" name="answer${i}" value="false" required>
-                <label for="answer${i}_false">Falso</label>
-            `;
+            var optionsDiv = document.createElement('div');
+            optionsDiv.classList.add('options');
+
+            var optionTrue = document.createElement('div');
+            var inputTrue = document.createElement('input');
+            inputTrue.type = 'radio';
+            inputTrue.id = 'answer' + i + '_true';
+            inputTrue.name = 'answer' + i;
+            inputTrue.value = 'true';
+            inputTrue.required = true;
+            var labelTrue = document.createElement('label');
+            labelTrue.textContent = 'Verdadero';
+            labelTrue.setAttribute('for', 'answer' + i + '_true');
+            optionTrue.appendChild(inputTrue);
+            optionTrue.appendChild(labelTrue);
+            optionsDiv.appendChild(optionTrue);
+
+            var optionFalse = document.createElement('div');
+            var inputFalse = document.createElement('input');
+            inputFalse.type = 'radio';
+            inputFalse.id = 'answer' + i + '_false';
+            inputFalse.name = 'answer' + i;
+            inputFalse.value = 'false';
+            inputFalse.required = true;
+            var labelFalse = document.createElement('label');
+            labelFalse.textContent = 'Falso';
+            labelFalse.setAttribute('for', 'answer' + i + '_false');
+            optionFalse.appendChild(inputFalse);
+            optionFalse.appendChild(labelFalse);
+            optionsDiv.appendChild(optionFalse);
+
+            questionDiv.appendChild(optionsDiv);
         } else if (questionType === 'multiplechoice') {
             var numOptions = parseInt(document.getElementById('numOptions' + i).value);
+            var optionsDiv = document.createElement('div');
+            optionsDiv.classList.add('options');
+
             for (var j = 1; j <= numOptions; j++) {
                 var optionText = document.getElementById('option' + j + '_' + i).value;
-                var optionId = 'option' + j + '_' + i;
-                questionDiv.innerHTML += `
-                    <input type="checkbox" id="${optionId}" name="${optionId}" value="${optionText}">
-                    <label for="${optionId}">${optionText}</label>
-                    <br>
-                `;
+                var optionDiv = document.createElement('div');
+
+                var optionInput = document.createElement('input');
+                optionInput.type = 'checkbox';
+                optionInput.id = 'option' + j + '_' + i;
+                optionInput.name = 'option' + j + '_' + i;
+                optionInput.value = optionText;
+
+                var optionLabel = document.createElement('label');
+                optionLabel.textContent = optionText;
+                optionLabel.setAttribute('for', 'option' + j + '_' + i);
+
+                optionDiv.appendChild(optionInput);
+                optionDiv.appendChild(optionLabel);
+                optionsDiv.appendChild(optionDiv);
             }
+
+            questionDiv.appendChild(optionsDiv);
         }
 
         answersForm.appendChild(questionDiv);
@@ -106,6 +161,7 @@ function submitForm() {
     // Mostrar solo el formulario de respuestas
     document.getElementById('responseForm').style.display = 'block';
 }
+
 
 
 
